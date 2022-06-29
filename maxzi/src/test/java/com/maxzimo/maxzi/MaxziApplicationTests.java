560ddecdf9cde774132e35ffa923b72e151a4dee
maxzimo <maxzimointernship@gmail.com>
2022-06-29 15:44:32 +0530

    adding maxzi folder

---
+++ maxzi/src/test/java/com/maxzimo/maxzi/MaxziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxzimo.maxzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
